61cad7856ae7f00aa37bacbdc0e90d784491b053
ShiroXXBlank <62727326+ShiroXXBlank@users.noreply.github.com>
2025-03-02 18:13:34 -0500

    Update lastVersion.lua

--- lastVersion.lua
+++ lastVersion.lua
@@ -211,7 +211,7 @@ if (localPlayer.PlayerGui:WaitForChild("Menu", 10) ~= nil) then
 
     game:GetService("Players").LocalPlayer.ClientNetwork:WaitForChild("MenuOptions"):FireServer(unpack(args)) -- Hit Character Slot
 
-    task.wait(10)
+    task.wait(15)
 
     local resource = "Platinum"
     resources = getResources(resource)
